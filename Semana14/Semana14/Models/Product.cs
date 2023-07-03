c3204e57e79f17dbd5f2f60501b6c1fd04e7ddcc
AldairAcosta1523 <86067289+AldairAcosta1523@users.noreply.github.com>
2023-07-02 23:59:09 -0500

    Agregar archivos de proyecto.

--- Semana14/Semana14/Models/Product.cs
+++ Semana14/Semana14/Models/Product.cs
@@ -7,7 +7,7 @@ namespace Semana14.Models
     public class Product
     {
         
-       public String Productosid { get; set; }
+       public String Productid { get; set; }
        public String Name { get; set; }
        public String Description { get; set; }
        public Double Price { get; set; }
